db03f28819e4aad671db3f60a2658a2850e0c799
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-15 05:21:00 +1000

    Define basic fetch quotes function

--- src/App.js
+++ src/App.js
@@ -5,14 +5,37 @@ class App extends React.Component {
 
   constructor() {
     super()
+    this.state = {
+      quote: "",
+      author: ""
+    }
+  }
+
+  componentDidMount() {
+    fetch(`https://api.quotable.io/random`)
+    .then((response) => {
+        if(response.status === 200) {
+          console.log(response);
+          return response.json();
+        }
+    }).then(data => {
+      console.log(data)
+      this.setState({
+        quote: data.content,
+        author: data.author
+      })
+    })
+    .catch((err) => {
+      console.log(`Error to fetch: ${err}`);
+    });
   }
 
   render() {
     return (
       <div className="App">
         <div id="quote-box">
-          <div id="text"></div>
-          <div id="author"></div>
+          <div id="text">{this.state.quote}</div>
+          <div id="author">{this.state.author}</div>
           <div id="new-quote">
           <a href="#" id="tweet-quote"></a></div>
         </div>
